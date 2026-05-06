65b0f870d9b05c9b223b56a5e698144093d156c9
jershot <jstebana0@gmail.com>
2026-05-06 09:20:42 -0500

    cambios tabo

--- vista/registro_emprendedores_vista.php
+++ vista/registro_emprendedores_vista.php
@@ -71,8 +71,8 @@
                             <div id="progreso_total-fill"></div>
                         </div>
 
-                        <div id="caja_progreso">
-                            <span id="progreso_no" class="step" data-step="1">1</span>
+                        <div id="caja_progreso" class="caja_progreso-steps">
+                            <span id="progreso_no" class="step active" data-step="1">1</span>
                             <span id="progreso_no" class="step" data-step="2">2</span>
                             <span id="progreso_no" class="step" data-step="3">3</span>
                             <span id="progreso_no" class="step" data-step="4">4</span>
